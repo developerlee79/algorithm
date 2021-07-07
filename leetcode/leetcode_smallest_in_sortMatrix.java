e0d301a9ccc298472ed3ff09306d772ffc5c87e6
devLee <81211112+developerlee79@users.noreply.github.com>
2021-07-07 21:27:56 +0900

    0707

---
+++ leetcode/leetcode_smallest_in_sortMatrix.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+
+public class leetcode_smallest_in_sortMatrix {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+        for(int[] R : matrix){
+            for(int a : R){
+                heap.add(a);
+                if(heap.size() > k)
+                    heap.poll();
+            }
+        }
+        return heap.peek();
+    }
+}
