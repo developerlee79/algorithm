99028b26be1ae02107d0dfea2b79b7cc23483fee
devLee <81211112+developerlee79@users.noreply.github.com>
2021-08-17 21:03:49 +0900

    0817

---
+++ leetcode/leetcode_inorder_traversal.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class leetcode_inorder_traversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+
+        while (cur != null || !stack.empty()) {
+            while (cur != null) {
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+
+        return list;
+    }
+}
