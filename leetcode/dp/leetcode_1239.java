af88e6f420ff151456f953dd23a6723914a0174e
devLee <81211112+developerlee79@users.noreply.github.com>
2021-09-22 19:59:09 +0900

    0922 Leetcode
    Daily Challenge. 생각보다 어려웠음

---
+++ leetcode/dp/leetcode_1239.java
@@ -0,0 +1,48 @@
+package leetcode.dp;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class leetcode_1239 {
+    public static void main(String[] args) {
+        leetcode_1239 lc = new leetcode_1239();
+
+        List<String> arr = new ArrayList<>();
+        arr.add("cha");
+        arr.add("r");
+        arr.add("act");
+        arr.add("ers");
+
+        System.out.println(lc.maxLength(arr));
+    }
+
+    public int maxLength(List<String> arr) {
+        return helper(arr, 0, "");
+    }
+
+    private int helper(List<String> arr, int index, String cur) {
+        if (index == arr.size()) return cur.length();
+
+        int res = cur.length();
+        for (int i = index; i < arr.size(); i++) {
+            String str = cur + arr.get(i);
+            if (isUnique(str)) {
+                res = Math.max(res, helper(arr, i + 1, str));
+            }
+
+        }
+        return res;
+    }
+
+    private boolean isUnique(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            if (!set.add(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
