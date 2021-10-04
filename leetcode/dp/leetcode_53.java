7b4d45adca8e329baad5d9a5cc6140901e9274c6
devLee <81211112+developerlee79@users.noreply.github.com>
2021-10-04 22:53:14 +0900

    1004
    Maximum Subarray / Easy / DP. 성능 분석법 공부가 필요하다

---
+++ leetcode/dp/leetcode_53.java
@@ -0,0 +1,23 @@
+package leetcode.dp;
+
+public class leetcode_53 {
+    public static void main(String[] args) {
+        leetcode_53 lc = new leetcode_53();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(lc.maxSubArray(nums));
+    }
+
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = nums[i] + (Math.max(dp[i - 1], 0));
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
