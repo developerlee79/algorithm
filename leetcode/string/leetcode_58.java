8d843538da7edb13c5d6f38cfdfe0475fdca7167
devle <81211112+developerlee79@users.noreply.github.com>
2021-10-26 21:50:07 +0900

    1026
    Length of Last Word / Easy

---
+++ leetcode/string/leetcode_58.java
@@ -0,0 +1,9 @@
+package leetcode.string;
+
+public class leetcode_58 {
+     int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ");
+
+        return arr[arr.length - 1].length();
+    }
+}
