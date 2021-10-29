1af7fad163833b1c2ce55cb88231af1295b70b83
devle <81211112+developerlee79@users.noreply.github.com>
2021-10-29 23:39:59 +0900

    1029
    Longest Common Prefix / Easy / String

---
+++ leetcode/string/leetcode_14.java
@@ -0,0 +1,18 @@
+package leetcode.string;
+
+public class leetcode_14 {
+    public static void main(String[] args) {
+        leetcode_14 lc = new leetcode_14();
+        String[] str = {"flower", "flow", "flight"};
+        System.out.println(lc.longestCommonPrefix(str));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String pre = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while(strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0, pre.length() - 1);
+        return pre;
+    }
+}
