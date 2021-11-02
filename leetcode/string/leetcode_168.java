c3324e619fdc8a775ec0abd0f005e3875d61ba09
devle <81211112+developerlee79@users.noreply.github.com>
2021-11-02 23:03:52 +0900

    1102
    Excel Sheet Column Title / Easy / String

---
+++ leetcode/string/leetcode_168.java
@@ -0,0 +1,12 @@
+package leetcode.string;
+
+public class leetcode_168 {
+    public static void main(String[] args) {
+        leetcode_168 lc = new leetcode_168();
+        System.out.println(lc.convertToTitle(28));
+    }
+
+    public String convertToTitle(int columnNumber) {
+        return columnNumber == 0 ? "" : convertToTitle(--columnNumber / 26) + (char) ('A' + (columnNumber % 26));
+    }
+}
