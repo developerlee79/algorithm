adf78f000b6bde3548e99fef6b4e82172400c2c3
devle <81211112+developerlee79@users.noreply.github.com>
2021-11-03 20:49:23 +0900

    1103
    Excel Sheet Column Number / Easy / String

---
+++ leetcode/string/leetcode_171.java
@@ -0,0 +1,18 @@
+package leetcode.string;
+
+public class leetcode_171 {
+    public static void main(String[] args) {
+        leetcode_171 lc = new leetcode_171();
+        System.out.println(lc.titleToNumber("ZY"));
+    }
+
+    public int titleToNumber(String columnTitle) {
+        if (columnTitle == null) return -1;
+        int sum = 0;
+        for (char c : columnTitle.toUpperCase().toCharArray()) {
+            sum *= 26;
+            sum += c - 'A' + 1;
+        }
+        return sum;
+    }
+}
