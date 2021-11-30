af927ee9fa48ea7bdd247d55912bd0b723fbcdf1
devle <81211112+developerlee79@users.noreply.github.com>
2021-11-30 22:31:19 +0900

    1130
    Sqrt(x) / Easy / Math

---
+++ leetcode/math/leetcode_69.java
@@ -0,0 +1,15 @@
+package leetcode.math;
+
+public class leetcode_69 {
+    public static void main(String[] args) {
+        leetcode_69 lc = new leetcode_69();
+        System.out.println(lc.mySqrt(8));
+    }
+
+    public int mySqrt(int x) {
+        long r = x;
+        while (r * r > x)
+            r = (r + x / r) / 2;
+        return (int) r;
+    }
+}
