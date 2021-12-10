6f781dce51599cb8bbdf6bcc96a660151ff18963
devle <81211112+developerlee79@users.noreply.github.com>
2021-12-10 23:23:33 +0900

    1210

---
+++ leetcode/array/leetcode_83.java
@@ -0,0 +1,26 @@
+package leetcode.array;
+
+public class leetcode_83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null)return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
