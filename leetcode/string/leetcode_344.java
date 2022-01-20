56610c4baacd0b80bb820251f6260d53b795e64e
devle <81211112+developerlee79@users.noreply.github.com>
2022-01-20 21:52:20 +0900

    [LeetCode - 334] 0120
    Reverse String / Easy / Array, String

---
+++ leetcode/string/leetcode_344.java
@@ -0,0 +1,26 @@
+package leetcode.string;
+
+import java.util.Arrays;
+
+public class leetcode_344 {
+    public static void main(String[] args) {
+        leetcode_344 lc = new leetcode_344();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        lc.reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+
+    // it has to be n(1) time complexity.
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for (int i = 0; i < (n / 2); i++) {
+            swap(s, i, n - i - 1);
+        }
+    }
+
+    private void swap(char[] c, int x, int y) {
+        char tmp = c[x];
+        c[x] = c[y];
+        c[y] = tmp;
+    }
+}
