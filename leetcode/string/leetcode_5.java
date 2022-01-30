699b8cfef0e08930cf59a7d152ea3e1c21f3e7a9
devle <81211112+developerlee79@users.noreply.github.com>
2022-01-30 23:35:43 +0900

    0103 UPDATE

--- leetcode/string/leetcode_5.java
+++ leetcode/string/leetcode_5.java
@@ -31,4 +31,23 @@ public class leetcode_5 {
         }
         return s.substring(begin, begin + maxLen);
     }
+    
+    public String longestPalindrome2(String s) {
+        int n = s.length();
+        String res = null;
+
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+        for (int j = i; j < n; j++) {
+          dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+          if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+            res = s.substring(i, j + 1);
+          }
+        }
+        }
+
+        return res;
+    }
 }
