a677c0b506f9ee6daae990570a4dc56bbd89b982
devle <81211112+developerlee79@users.noreply.github.com>
2022-02-17 22:39:07 +0900

    [LeetCode - 1399] 0217
    Count Largest Group / Easy / Math

---
+++ leetcode/math/leetcode_1399.java
@@ -0,0 +1,32 @@
+package leetcode.math;
+
+public class leetcode_1399 {
+    public static void main(String[] args) {
+        System.out.println(countLargestGroup(24));
+    }
+
+    public static int countLargestGroup(int n) {
+        int[] dp = new int[37];
+        int max = 0, cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = sum(i);
+            dp[sum]++;
+            max = Math.max(max, dp[sum]);
+        }
+
+        for (int i = 0; i < 37; i++) {
+            if (dp[i] == max) cnt++;
+        }
+
+        return cnt;
+    }
+
+    private static int sum(int n) {
+        int s = 0;
+        while (n > 0) {
+            s = s + n % 10;
+            n = n / 10;
+        }
+        return s;
+    }
+}
