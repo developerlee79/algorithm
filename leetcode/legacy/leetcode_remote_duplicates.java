fca266257575588ef8dba6a1221df761deaadb29
devle <81211112+developerlee79@users.noreply.github.com>
2022-02-20 01:54:02 +0900

    [LeetCode] Delete legacy directory
    Date : 2022-02-20

--- leetcode/legacy/leetcode_remote_duplicates.java
+++
@@ -1,9 +0,0 @@
-public class leetcode_remote_duplicates {
-    public int removeDuplicates(int[] nums) {
-        int i = 0;
-        for (int n : nums)
-            if (i == 0 || n > nums[i - 1])
-                nums[i++] = n;
-        return i;
-    }
-}
