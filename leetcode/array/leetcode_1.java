88d89ab53af448aac3d0351d0e7d4dbdd6dc1499
devle <81211112+developerlee79@users.noreply.github.com>
2022-03-05 03:32:38 +0900

    [LeetCode - 1] Two Sum
    Title : Two Sum
    Topic : Array, Hash Table
    Difficulty : Easy
    Date : 2022-03-05

--- leetcode/array/leetcode_1.java
+++ leetcode/array/leetcode_1.java
@@ -1,36 +1,47 @@
 package leetcode.array;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class leetcode_1 {
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        
-        return new int[]{0, 0};
+    public static void main(String[] args) {
+        leetcode_1 lc = new leetcode_1();
+        int[] nums = {2, 5, 5, 11};
+        System.out.println(Arrays.toString(lc.twoSum(nums, 10)));
+        System.out.println(Arrays.toString(lc.twoSumBrute(nums, 10)));
     }
-    
-    public int[] twoSum2(int[] nums, int target) {
-        int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+    // Solution - HashMap
+    // Time Complexity - O(N)
+    // Space Complexity - O(N)
+    // Put target - nums[i] value at hashmap.
+    // If map contains nums[i], it means nums[i] + map[nums[i]] == target
+    // Because target - nums[i] always has to be correct answer.
+    // return value of map & current index as array.
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                result[1] = i + 1;
-                result[0] = map.get(target - nums[i]);
-                return result;
-            }
-
-            if(map.containsKey(nums[i])) {
-               continue;
-            }
-
-            map.put(nums[i], i + 1);
+            if (map.containsKey(nums[i]))
+                return new int[]{map.get(nums[i]), i};
+            map.put((target - nums[i]), i);
         }
-        return result;
+
+        return null;
+    }
+
+    // Solution - Brute force
+    // Time Complexity - O(N^2)
+    // Space Complexity - O(N)
+    // iterate until found target value
+    public int[] twoSumBrute(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) continue;
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
+            }
+        }
+
+        return null;
     }
 }
