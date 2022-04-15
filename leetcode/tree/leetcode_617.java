8e5869d29d6ec9b4150e45eae179a956f91dbdb8
lh <dltjgus0709@gmail.com>
2022-04-15 22:05:16 +0900

    [LeetCode - 617] Merge Two Binary Trees Title : Merge Two Binary Trees Topic : Tree, Depth-First Search, Binary Tree Difficulty : Easy Date : 2022-04-15

---
+++ leetcode/tree/leetcode_617.java
@@ -0,0 +1,60 @@
+package tree;
+
+public class leetcode_617 {
+    public static void main(String[] args) {
+        leetcode_617 leetcode = new leetcode_617();
+        TreeNode root1 = new TreeNode(1, new TreeNode(3, new TreeNode(5), null), new TreeNode(2));
+        TreeNode root2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+        System.out.println(leetcode.mergeTrees(root1, root2).toString());
+    }
+
+    // Solution - DFS
+    // Time Complexity : O(N)
+    // Space Complexity : O(N)
+    // Simple DFS
+    // Because of it's MERGE trees, so if one tree does not exist, after then only the other tree will exist.
+    // Other than that, just go with DFS.
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return null;
+        } else if (root1 == null) {
+            return root2;
+        } else if (root2 == null) {
+            return root1;
+        }
+
+        root1.val = root1.val + root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+
+        return root1;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+}
