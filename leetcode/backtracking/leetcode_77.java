4d947c01f171bd95a48432acca75e738e95cc13a
lh <dltjgus0709@gmail.com>
2022-05-02 23:02:01 +0900

    [LeetCode - 77] Combinations Title : Combinations Topic : Backtracking Difficulty : Medium Date : 2022-05-02

---
+++ leetcode/backtracking/leetcode_77.java
@@ -0,0 +1,34 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode_77 {
+    public static void main(String[] args) {
+        leetcode_77 leetcode = new leetcode_77();
+        int n = 4, k = 2;
+        System.out.println(leetcode.combine(n, k));
+    }
+
+    // Solution - Backtracking
+    // Time Complexity : O(N)
+    // Space Complexity : O(1)
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> resultArray = new ArrayList<>();
+        backtracking(resultArray, new ArrayList<>(), n, k, 1);
+        return resultArray;
+    }
+
+    void backtracking(List<List<Integer>> resultArray, List<Integer> elementArray, int n, int k, int i) {
+        if (elementArray.size() == k) {
+            resultArray.add(new ArrayList<>(elementArray));
+            return;
+        }
+
+        for (; i <= n; i++) {
+            elementArray.add(i);
+            backtracking(resultArray, elementArray, n, k, i + 1);
+            elementArray.remove(elementArray.size() - 1);
+        }
+    }
+}
