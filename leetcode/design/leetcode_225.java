1017a2310831898e3531ad8d6035cea883672eda
lh <dltjgus0709@gmail.com>
2022-05-05 23:46:07 +0900

    [LeetCode - 225] Implement Stack using Queues Title : Implement Stack using Queues Topic : Stack, Design, Queue Difficulty : Easy Date : 2022-05-05

---
+++ leetcode/design/leetcode_225.java
@@ -0,0 +1,44 @@
+package leetcode.design;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class leetcode_225 {
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        myStack.push(1);
+        myStack.push(2);
+        System.out.println(myStack.top());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.empty());
+    }
+
+    // Solution - Linked list
+    static class MyStack {
+
+        private final Queue<Integer> stackQueue;
+
+        public MyStack() {
+            stackQueue = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            stackQueue.add(x);
+
+            for (int i = 1; i < stackQueue.size(); i++)
+                stackQueue.add(stackQueue.remove());
+        }
+
+        public int pop() {
+            return stackQueue.remove();
+        }
+
+        public int top() {
+            return stackQueue.peek();
+        }
+
+        public boolean empty() {
+            return stackQueue.isEmpty();
+        }
+    }
+}
