3cdf73c574cb057af32174a3db3553f26eded72f
devle <dltjgus0709@gmail.com>
2023-06-09 22:36:23 +0900

    [LeetCode - 744] Find Smallest Letter Greater Than Target
    Topics : Array, Binary Search
    Difficulty : Easy
    Solution : https://leetcode.com/problems/find-smallest-letter-greater-than-target/solutions/3618334/kotlin-binary-search/

---
+++ search/LeetCode_744.kt
@@ -0,0 +1,38 @@
+package search
+
+class LeetCode_744 {
+
+    fun nextGreatestLetter(letters: CharArray, target: Char): Char {
+        val n = letters.size
+
+        if (target >= letters.last()) {
+            return letters.first()
+        }
+
+        var left = 0
+        var right = n - 1
+
+        while (left <= right) {
+            val mid = (left + right) / 2
+
+            if (letters[mid] <= target) {
+                left = mid - 1
+            } else {
+                right = mid
+            }
+        }
+
+        return letters[right]
+    }
+}
+
+fun main() {
+    // Input
+    val letters = charArrayOf('c', 'f', 'j')
+    val target = 'a'
+    // Output
+    LeetCode_744().nextGreatestLetter(letters, target).run {
+        println(this)
+        require('c' == this)
+    }
+}
