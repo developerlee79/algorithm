c7887148139770e3bf521a558a422da39c4dc173
devle <dltjgus0709@gmail.com>
2023-08-15 14:52:36 +0900

    [LeetCode - 86] Partition List
    Topics : Linked List, Two Pointers
    Difficulty : Medium
    Solution :

--- linked_list/LeetCode_86.kt
+++ linked_list/LeetCode_86.kt
@@ -1,23 +1,9 @@
 package linked_list
 
+import util.ListNode
+
 class LeetCode_86 {
 
-    data class ListNode(var `val`: Int) {
-        var next: ListNode? = null
-    }
-
-    /**
-     * Solution - Two Pointers
-     * Time Complexity : O(N)
-     * Space Complexity : O(N)
-     *
-     * This problem is simply sorting the Linked List by X.
-     * Therefore, we can combine a linked list less than X and a linked list greater than or equal to X to obtain a partition list.
-     *
-     * @param head head of a linked list
-     * @param x integer
-     * @return parted array such that all node less than x come before nodes greater than or equal to x
-    */
     fun partition(head: ListNode?, x: Int): ListNode? {
         val smallerList = ListNode(-1)
         val greaterList = ListNode(-1)
@@ -44,12 +30,12 @@ class LeetCode_86 {
 }
 
 fun main() {
-    val head = LeetCode_86.ListNode(1)
-    head.next = LeetCode_86.ListNode(4)
-    head.next!!.next = LeetCode_86.ListNode(3)
-    head.next!!.next!!.next = LeetCode_86.ListNode(2)
-    head.next!!.next!!.next!!.next = LeetCode_86.ListNode(5)
-    head.next!!.next!!.next!!.next!!.next = LeetCode_86.ListNode(2)
+    val head = ListNode.create(1, 4, 3, 2, 5, 2)
     val x = 3
-    println(LeetCode_86().partition(head, x).toString())
+    LeetCode_86().partition(head, x).run {
+        println(this)
+        require(
+            ListNode.create(1,2,2,4,3,5)!!.equals(this)
+        )
+    }
 }
