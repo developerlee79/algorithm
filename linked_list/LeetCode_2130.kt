e1b1cfa6d359251d2bd2e14a01d7cbdedd6ede89
devle <dltjgus0709@gmail.com>
2023-09-12 20:32:52 +0900

    [LeetCode - 2130] Maximum Twin Sum of a Linked List
    Topics : Linked List, Two Pointers, Stack
    Difficulty : Medium

--- linked_list/LeetCode_2130.kt
+++ linked_list/LeetCode_2130.kt
@@ -1,50 +1,23 @@
 package linked_list
 
 import util.ListNode
-import java.util.Deque
-import java.util.LinkedList
-import java.util.Stack
 
 class LeetCode_2130 {
 
-    private fun Deque<Int>.pollEach() = this.pollFirst() + this.pollLast()
-
     fun pairSum(head: ListNode?): Int {
-        val deQueue: Deque<Int> = LinkedList()
+        val nodeDeque = ArrayDeque<Int>()
 
         var head = head
 
         while (head != null) {
-            deQueue.add(head.`val`)
+            nodeDeque.add(head.`val`)
             head = head.next
         }
 
         var maxSum = 0
 
-        while (deQueue.isNotEmpty()) {
-            maxSum = maxOf(maxSum, deQueue.pollEach())
-        }
-
-        return maxSum
-    }
-
-    fun pairSumStack(head: ListNode?): Int {
-        val stack = Stack<Int>()
-
-        var slowHead = head
-        var fastHead = head
-
-        while (slowHead != null && fastHead?.next != null) {
-            stack.add(slowHead.`val`)
-            slowHead = slowHead.next
-            fastHead = fastHead.next?.next
-        }
-
-        var maxSum = 0
-
-        while (slowHead != null) {
-            maxSum = maxOf(maxSum, stack.pop() + slowHead.`val`)
-            slowHead = slowHead.next
+        while (nodeDeque.isNotEmpty()) {
+            maxSum = maxOf(maxSum, nodeDeque.removeFirst() + nodeDeque.removeLast())
         }
 
         return maxSum
