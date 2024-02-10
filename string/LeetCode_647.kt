564d360f5a3303b8f65f4411ab0373d6632c28a9
devle <dltjgus0709@gmail.com>
2024-02-10 21:08:19 +0900

    647. Palindromic Substrings
    Topics : String, Dynamic Programming
    Difficulty : Medium

--- string/LeetCode_647.kt
+++ string/LeetCode_647.kt
@@ -1,44 +1,42 @@
-package string;
+package string
 
-public class LeetCode_647 {
-    public static void main(String[] args) {
-        LeetCode_647 leetcode = new LeetCode_647();
-        String s = "abc";
-        System.out.println(leetcode.countSubstrings(s));
+class LeetCode_647 {
+
+    fun countSubstrings(s: String): Int {
+        if (s.isEmpty()) {
+            return 0
+        }
+
+        var result = 0
+        val chars = s.toCharArray()
+
+        for (i in chars.indices) {
+            result += countPalindrome(chars, i, i)
+            result += countPalindrome(chars, i, i + 1)
+        }
+
+        return result
     }
 
-    /**
-     * @param s target string
-     * @return number of palindrome strings
-     *
-     * Solution - Dynamic programming
-     * Time Complexity : O(N)
-     * Space Complexity : O(1)
-     *
-     * Find the total number by making sure it is a palindrome string for i, i + 1 target in every iteration.
-     */
-    public int countSubstrings(String s) {
-        if (s.length() == 0) {
-            return 0;
+    private fun countPalindrome(chars: CharArray, index: Int, next: Int): Int {
+        var index = index
+        var next = next
+        var count = 0
+
+        while (index >= 0 && next < chars.size && chars[index--] == chars[next++]) {
+            count++
         }
 
-        int result = 0;
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            result += isPalindrome(chars, i, i);
-            result += isPalindrome(chars, i, i + 1);
-        }
-
-        return result;
+        return count
     }
+}
 
-    private int isPalindrome(char[] chars, int index, int next) {
-        int count = 0;
-
-        while (index >= 0 && next < chars.length && chars[index--] == chars[next++]) {
-            count++;
-        }
-
-        return count;
+fun main() {
+    // Input
+    val s = "abc"
+    // Output
+    LeetCode_647().countSubstrings(s).run {
+        println(this)
+        require(3 == this)
     }
 }
